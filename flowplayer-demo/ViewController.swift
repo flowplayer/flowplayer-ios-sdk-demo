592db4e277872f706204bd9e7493759b07037193
Georgios Savvidis <george.savvidis@thecodeyard.com>
2019-09-01 09:47:24 +0200

    Added initial iOS project

---
+++ flowplayer-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flowplayer-demo
+//
+//  Created by Georgios Savvidis on 01.09.19.
+//  Copyright © 2019 Flowplayer AB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
